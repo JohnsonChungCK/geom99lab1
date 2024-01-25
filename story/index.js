01ed37e07882539253422976109692f91e8ff793
JohnsonChungCK <146375963+JohnsonChungCK@users.noreply.github.com>
2024-01-25 13:23:29 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -4,11 +4,11 @@ function initialize() {
   const Banff = { lat: 51.484163, lng: -116.081291 };
   const map = new google.maps.Map(document.getElementById("map1"), {
     center: Banff,
-    zoom: 10,
+    zoom: 8,
   });
   const panorama = new google.maps.StreetViewPanorama(
     document.getElementById("pano"),
-    {position: Banff,pov: {heading: 34, pitch: 10,},},    
+    {position: Banff,pov: {heading: 20, pitch: 20,},},    
   );
   
   map.setStreetView(panorama);
