7d8be43ad5550200a85ca2afd7e4558e6c021526
JohnsonChungCK <146375963+JohnsonChungCK@users.noreply.github.com>
2024-01-25 09:32:20 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -10,14 +10,22 @@ function initialize() {
     document.getElementById("pano"),
     {position: Banff,pov: {heading: 34, pitch: 10,},},    
   );
- 
-  const myLatLng = { lat: 51.464097, lng: -116.577728 };
-  
-  new google.maps.Marker({
-    position: myLatLng, map1, title: "Yoho National Park Of Canada",
-  });
   
   map.setStreetView(panorama);
+}
+
+function initMap() {
+  const myLatLng = { lat: 51.464097, lng: -116.577728 };
+  const map2 = new google.maps.Map(document.getElementById("map2"), {
+    zoom: 4,
+    center: myLatLng,
+  });
+
+  new google.maps.Marker({
+    position: myLatLng,
+    map2,
+    title: "Hello World!",
+  });
 }
 
 window.initialize = initialize;
