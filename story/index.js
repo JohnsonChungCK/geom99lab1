5c5cf1cb64893f2f25b41cb932cbbbf7d7e02144
JohnsonChungCK <146375963+JohnsonChungCK@users.noreply.github.com>
2024-01-25 08:33:22 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -1,10 +1,10 @@
 
 
 function initialize() {
-  const fenway = { lat: 42.345573, lng: -71.098326 };
+  const fenway = { lat: 51.484163, lng: -116.081291 };
   const map = new google.maps.Map(document.getElementById("map1"), {
-    center: fenway,
-    zoom: 14,
+    center: Banff,
+    zoom: 10,
   });
   const panorama = new google.maps.StreetViewPanorama(
     document.getElementById("pano"),
